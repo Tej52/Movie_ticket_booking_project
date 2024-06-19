f2788fb72aa6dd874e3a89a1a06f0027c6d28906
Harsha-0031 <harsha.n.chari31@gmail.com>
2024-06-19 23:46:51 +0530

    Branch Harsha is created and the project was done in new folder call Harsha

---
+++ Harsha/app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, render_template 
+
+app = Flask(__name__)
+@app.route('/', methods = ['GET', 'POST'])
+def index():
+    print( request.method)
+    req = request.method
+    if req == 'GET':
+        return render_template('index.html')
+    else:
+        return 'POST REQ'
+@app.route('/login', methods = ["GET", "POST"])
+def login():
+    req = request.method
+    if req == 'GET':
+        return render_template('login.html')
+
+
+@app.route('/user', methods = ['POST', 'GET'])
+def user():
+    req = request.method 
+    if req == 'POST':
+        return "User Page"
+    
+@app.route('/creat', methods = ['GET', 'POST'])
+def creat():
+    req = request.method 
+    if req == 'GET':
+        return render_template('creat.html')
+    else:
+        return "Hello"
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)
